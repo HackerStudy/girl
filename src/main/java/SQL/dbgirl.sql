2ab2fe60080f31beb423e3cfd02988fe2e636157
HackerStudy <1791133899@qq.com>
2018-10-20 11:06:26 +0800

    【新增】 更新数据库文件

--- src/main/java/SQL/dbgirl.sql
+++ src/main/java/SQL/dbgirl.sql
@@ -1,39 +1,48 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Mysql
-Source Server Version : 50716
+Source Server         : mysql
+Source Server Version : 50640
 Source Host           : localhost:3306
 Source Database       : dbgirl
 
 Target Server Type    : MYSQL
-Target Server Version : 50716
+Target Server Version : 50640
 File Encoding         : 65001
 
-Date: 2018-10-18 00:20:08
+Date: 2018-10-20 11:03:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `girl`
+-- Table structure for girl
 -- ----------------------------
 DROP TABLE IF EXISTS `girl`;
 CREATE TABLE `girl` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `age` int(11) DEFAULT NULL,
-  `cup_size` varchar(255) DEFAULT NULL,
+  `cup_size` varchar(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of girl
 -- ----------------------------
 INSERT INTO `girl` VALUES ('1', '18', 'B');
 INSERT INTO `girl` VALUES ('2', '22', 'D');
+INSERT INTO `girl` VALUES ('4', '22', 'D');
+INSERT INTO `girl` VALUES ('5', '22', 'D');
+INSERT INTO `girl` VALUES ('6', '18', 'B');
+INSERT INTO `girl` VALUES ('7', '1', 'B');
+INSERT INTO `girl` VALUES ('8', '22', 'D');
+INSERT INTO `girl` VALUES ('9', '20', 'B');
+INSERT INTO `girl` VALUES ('12', '20', 'B');
+INSERT INTO `girl` VALUES ('14', '20', 'B');
+INSERT INTO `girl` VALUES ('16', '20', 'B');
 
 -- ----------------------------
--- Table structure for `hibernate_sequence`
+-- Table structure for hibernate_sequence
 -- ----------------------------
 DROP TABLE IF EXISTS `hibernate_sequence`;
 CREATE TABLE `hibernate_sequence` (
@@ -43,4 +52,4 @@ CREATE TABLE `hibernate_sequence` (
 -- ----------------------------
 -- Records of hibernate_sequence
 -- ----------------------------
-INSERT INTO `hibernate_sequence` VALUES ('4');
+INSERT INTO `hibernate_sequence` VALUES ('18');
