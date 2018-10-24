a1c90fe1048889132e92ef4509579c88f571cd73
HackerStudy <1791133899@qq.com>
2018-10-24 17:51:56 +0800

    【新增】将打包方式改成war包

---
+++ src/main/java/com/yangpeng/girl/GirlApplicationWar.java
@@ -0,0 +1,22 @@
+package com.yangpeng.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Springboot的启动类(打包方式为war)
+ */
+@SpringBootApplication
+public class GirlApplicationWar extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlApplicationWar.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(GirlApplicationWar.class);
+    }
+}
