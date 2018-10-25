b4a34ead095917d5562b5b9712e2ce8a50c54ea1
HackerStudy <1791133899@qq.com>
2018-10-25 19:39:48 +0800

    【新增】 数据库事务回滚注解

--- src/test/java/com/yangpeng/girl/GirlApplicationTests.java
+++ src/test/java/com/yangpeng/girl/GirlApplicationTests.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 测试的基类
@@ -17,6 +18,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringRunner.class) //表示使用Spring Test组件进行单元测试，其中SpringRunner继承类SpringJUnit4ClassRunner。
 @SpringBootTest //加载整个spring工程
 @WebAppConfiguration //由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+//配置事务的回滚,对数据库的增删改都会回滚,便于测试用例的循环利用
+//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+//@Transactional
 public class GirlApplicationTests {
     /** slf4日志*/
     public static final Logger logger = LoggerFactory.getLogger(GirlServiceTest.class);
