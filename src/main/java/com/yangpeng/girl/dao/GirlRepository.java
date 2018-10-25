a763ef3d2a5df7dddb7ddb4a4e73d3b3160908d8
HackerStudy <1791133899@qq.com>
2018-10-25 11:33:03 +0800

    【新增】dao层接口表示，pom.xml中war的命名

--- src/main/java/com/yangpeng/girl/dao/GirlRepository.java
+++ src/main/java/com/yangpeng/girl/dao/GirlRepository.java
@@ -2,12 +2,15 @@ package com.yangpeng.girl.dao;
 
 import com.yangpeng.girl.entity.Girl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * girl的dao层接口，继承jpa（里面实体类为Girl,id主键为Integer类型）
  */
+//@Repository
+//@RepositoryDefinition(domainClass=Girl.class,idClass=Integer.class)
 public interface GirlRepository extends JpaRepository<Girl,Integer>{
     /**
      * 按年龄查找
@@ -15,4 +18,6 @@ public interface GirlRepository extends JpaRepository<Girl,Integer>{
      * @return
      */
     List<Girl> findByAge(Integer age);
+
+
 }
